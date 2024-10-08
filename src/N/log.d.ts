2b8be27e6262b26130b5b117d4f59405bff0a364
Bernardo Tonasse <bernardo.tonasse@oracle.com>
2024-10-08 11:30:32 +0200

    update global declarations

--- src/N/log.d.ts
+++ src/N/log.d.ts
@@ -1,4 +1,5 @@
-interface LogOptions {
+export declare namespace log {
+  interface LogOptions {
     /** String to appear in the Title column on the Execution Log tab of the script deployment. Maximum length is 99 characters. */
     title?: string;
     /**
@@ -7,14 +8,22 @@ interface LogOptions {
      * NetSuite truncates any resulting string over 3999 characters.
      */
     details: any;
-}
+  }
 
-interface LogFunction {
+  interface LogFunction {
     (title: string, details: any): void;
     (options: LogOptions): void;
-}
+  }
 
-export const debug: LogFunction;
-export const audit: LogFunction;
-export const error: LogFunction;
-export const emergency: LogFunction;
+  function debug(title: string, details: any): void;
+  function debug(options: LogOptions): void;
+
+  function audit(title: string, details: any): void;
+  function audit(options: LogOptions): void;
+
+  function error(title: string, details: any): void;
+  function error(options: LogOptions): void;
+
+  function emergency(title: string, details: any): void;
+  function emergency(options: LogOptions): void;
+}
